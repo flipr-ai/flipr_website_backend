b1b426cf3971e7432c9737a254c14e2c10b46835
kishan-flipr <kishan@flipr.ai>
2019-06-05 19:03:24 +0530

    added code for paytm

--- routes/admin/response.js
+++ routes/admin/response.js
@@ -6,18 +6,26 @@ var config = require('../../config/config');
 router.post('/response', function (req, res) {
   // console.log("in response post");
   var paramlist = req.body;
+  const newbody = new Array();
+  newbody['ORDER_ID'] = req.body['ORDER_ID'];
+  newbody['CUST_ID'] = req.body['CUST_ID'];
+  newbody['MID'] = req.body['MID'];
+  newbody['CHANNEL_ID'] = req.body['CHANNEL_ID'];
+  newbody['TXN_AMOUNT'] = req.body['TXN_AMOUNT'];
+  newbody['CHECKSUMHASH'] = req.body['CHECKSUMHASH'];
 
   //var paramarray = new Array();
   console.log("parapmeter list");
   console.log(paramlist);
-  if (checksum.verifychecksum(paramlist, config.PAYTM_MERCHANT_KEY)) {
+  console.log(newbody);
+  if (checksum.verifychecksum(newbody, config.PAYTM_MERCHANT_KEY)) {
 
     console.log("true");
-    console.log(paramlist);
+//    console.log(paramlist);
     res.render('response.ejs', { 'restdata': "true", 'paramlist': paramlist });
   } else {
     console.log("false");
-    console.log(paramlist);
+  //  console.log(paramlist);
     res.render('response.ejs', { 'restdata': "false", 'paramlist': paramlist });
   };
   //vidisha
